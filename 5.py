23374c26e80cfb02b30842a62bbc770b7005e149
Hamid Habib Syed <122344493+HamidSyed298@users.noreply.github.com>
2026-06-08 10:00:55 -0500

    Reformatting

--- 5.py
+++ 5.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-#okay
+
 img = cv2.resize(cv2.imread('images/soccer_practice.jpg', 0), (0, 0), fx=0.8, fy=0.8)
 template = cv2.resize(cv2.imread('images/shoe.PNG', 0), (0, 0), fx=0.8, fy=0.8)
 h, w = template.shape
