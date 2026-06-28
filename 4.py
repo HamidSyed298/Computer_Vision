f8a153cfdc45606e719f1e15dc8b0db57f0fb574
Hamid Habib Syed <122344493+HamidSyed298@users.noreply.github.com>
2026-06-28 12:34:44 -0500

    Refactor
    Added a print statement for corners detected.

--- 4.py
+++ 4.py
@@ -19,7 +19,7 @@ for i in range(len(corners)):
 		corner2 = tuple(corners[j][0])
 		color = tuple(map(lambda x: int(x), np.random.randint(0,255, size = 3)))
 		cv2.line(img, corner1, corner2,(),1)
-
+#printing
 print(corners)
 
 
